c8e02160b6bf3c58c587a0e81a98dcd95232c98c
Leslie Sachs <lsachs7565@gmail.com>
2015-12-14 19:08:21 -0500

    First commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,11 @@
+$("#main").append("Leslie Sachs");
+
+var name = "Leslie Sachs";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
